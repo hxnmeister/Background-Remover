3d0a4e51f0d91e8e139789dad2ad91c6734d7dd3
Valerii <valeriy.stronskiy@gmail.com>
2024-05-06 09:47:46 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Background_Remover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageLoaderButton = new System.Windows.Forms.Button();
+            this.PreviewPictureBox = new System.Windows.Forms.PictureBox();
+            this.RemoveBackgButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageLoaderButton
+            // 
+            this.ImageLoaderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.ImageLoaderButton.Location = new System.Drawing.Point(12, 359);
+            this.ImageLoaderButton.Name = "ImageLoaderButton";
+            this.ImageLoaderButton.Size = new System.Drawing.Size(94, 43);
+            this.ImageLoaderButton.TabIndex = 0;
+            this.ImageLoaderButton.Text = "Load File";
+            this.ImageLoaderButton.UseVisualStyleBackColor = true;
+            this.ImageLoaderButton.Click += new System.EventHandler(this.ImageLoaderButton_Click);
+            // 
+            // PreviewPictureBox
+            // 
+            this.PreviewPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PreviewPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.PreviewPictureBox.Name = "PreviewPictureBox";
+            this.PreviewPictureBox.Size = new System.Drawing.Size(525, 288);
+            this.PreviewPictureBox.TabIndex = 1;
+            this.PreviewPictureBox.TabStop = false;
+            // 
+            // RemoveBackgButton
+            // 
+            this.RemoveBackgButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.RemoveBackgButton.Enabled = false;
+            this.RemoveBackgButton.Location = new System.Drawing.Point(443, 359);
+            this.RemoveBackgButton.Name = "RemoveBackgButton";
+            this.RemoveBackgButton.Size = new System.Drawing.Size(94, 43);
+            this.RemoveBackgButton.TabIndex = 2;
+            this.RemoveBackgButton.Text = "Remove Background";
+            this.RemoveBackgButton.UseVisualStyleBackColor = true;
+            this.RemoveBackgButton.Click += new System.EventHandler(this.RemoveBackgButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 414);
+            this.Controls.Add(this.RemoveBackgButton);
+            this.Controls.Add(this.PreviewPictureBox);
+            this.Controls.Add(this.ImageLoaderButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Background Remover";
+            ((System.ComponentModel.ISupportInitialize)(this.PreviewPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ImageLoaderButton;
+        private System.Windows.Forms.PictureBox PreviewPictureBox;
+        private System.Windows.Forms.Button RemoveBackgButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace Background_Remover
+{
+    public partial class Form1 : Form
+    {
+        private string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageLoaderButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = "Images (*.jpg, *.png, *.jpeg)|*.jpg; *.png; *.jpeg"
+            };
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = fileDialog.FileName;
+                PreviewPictureBox.Image = ScaleImageToFit(Image.FromFile(fileName), PreviewPictureBox);
+                RemoveBackgButton.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Image loading is canceled!", "Canceled!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void RemoveBackgButton_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                formData.Headers.Add("X-Api-Key", "7JvYMMWzRBgHB1Sj7t4ndLob");
+                formData.Add(new ByteArrayContent(File.ReadAllBytes(fileName)), "image_file", "file.jpg");
+                formData.Add(new StringContent("auto"), "size");
+                var response = client.PostAsync("https://api.remove.bg/v1.0/removebg", formData).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string pathToDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    FileStream fileStream = new FileStream(pathToDocuments + "\\no-bg.png", FileMode.Create, FileAccess.Write, FileShare.None);
+                    response.Content.CopyToAsync(fileStream).ContinueWith((copyTask) => { fileStream.Close(); });
+                    RemoveBackgButton.Enabled = false;
+                    PreviewPictureBox.Image = null;
+                    Process.Start("explorer.exe", pathToDocuments);
+                    MessageBox.Show("Background successfully removed!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("response.Content.ReadAsStringAsync().Result!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private Image ScaleImageToFit(Image image, PictureBox pictureBox)
+        {
+            float ratio = Math.Min((float)pictureBox.Width / image.Width, (float)pictureBox.Height / image.Height);
+            int newWidth = (int)(image.Width * ratio);
+            int newHeight = (int)(image.Height * ratio);
+            return new Bitmap(image, new Size(newWidth, newHeight));
+        }
+    }
+}
